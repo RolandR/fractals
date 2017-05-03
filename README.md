52fa8ebf9b80d876856950db7d403f579382ba2e
Roland Rytz <roland.rytz@gmail.com>
2017-05-03 18:29:52 +0200

    Add license and some meta tags

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# Fractal Stuff
+# Lindenmayer Systems in Javascript
 
-Demo: http://rolandrytz.ch/various/fractals/
+This project contains various experiments with L-Systems,
+most notably an [online sandbox for arbitrary L-System rules](https://draemm.li/various/fractals/l-systems/).
+
+An overview over the other tools is [available here](http://draemm.li/various/fractals/).
+
+---
+
+![Screenshot](https://draemm.li/various/fractals/screenshot.png)
+
+## Usage
+
+The [L-System Sandbox](https://draemm.li/various/fractals/l-systems/) contains instructions to defining rules for drawing L-Systems.  
+You can share sets of rules by clicking the "Export" button and copying either the generated link or the generated JSON data.
+
+## License
+
+Copyright 2016, 2017 by Roland Rytz.
+All code is licensed unter the free [AGPL v3](https://draemm.li/various/fractals/LICENSE) license.
--- l-systems/l-systems.js
+++ l-systems/l-systems.js
@@ -1,60 +1,29 @@
+
+
+
 /*
-
-cool rules:
-
-[
-  2,
-  "A",
-  "A > A-.A.-A",
-  60,
-  5,
-  7,
-  0.319,
-  0.656,
-  90
-]
-
-
-[
-  2,
-  "B",
-  "B > B+B---BB+++B-B",
-  60,
-  6,
-  7,
-  0.429,
-  0.744,
-  0
-]
-
-[
-  2,
-  ".A",
-  "A > A+A-A--.A.++A",
-  60,
-  5,
-  5,
-  0.146,
-  0.607,
-  90
-]
-
-[
-  2,
-  "A+.A--.A+.A",
-  "A > A+A-A--.A.++A",
-  60,
-  3,
-  5,
-  0.272,
-  0.218,
-  90
-]
-
-
-
-
+	========================================================================
+	L-Systems Sandbox
+	
+	An online sandbox for experimenting with Lindenmayer Systems.
+	
+	Copyright (C) 2016, 2017 Roland Rytz <roland@draemm.li>
+	
+	Licensed under the GNU Affero General Public License Version 3
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	For more information, see:
+	https://draemm.li/various/fractals/LICENSE
+	
+	========================================================================
 */
+
+
 var popup = null;
 function closePopup(){
 	if(popup){
